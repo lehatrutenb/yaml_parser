70419ad1851ac11c49c5d44a9ac97f78292a6cc5
Алексей Маренков <lehatrutenb@gmail.com>
2023-12-22 22:44:07 +0300

    Make just server config file parser

--- yaml_parser.go
+++ yaml_parser.go
@@ -1,22 +1,36 @@
-package yamlparser
+package yaml_parser
 
 import (
 	"os"
+	"time"
 
 	"gopkg.in/yaml.v2"
 )
 
-func ParseYamlFile(obj *any, filePath string) error {
-	b, err := os.ReadFile(filePath)
-	if err != nil {
-		return err
-	}
-
-	return ParseYaml(obj, b)
+type Config struct {
+	Server ServerConfig
 }
 
-func ParseYaml(obj *any, b []byte) error {
+type ServerConfig struct {
+	Host    string
+	Port    int
+	Timeout time.Duration `yaml:"timeout,omitempty"`
+}
+
+func ParseYamlFile(filePath string) (Config, error) {
+	b, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return ParseYaml(b)
+}
+
+func ParseYaml(b []byte) (Config, error) {
 	b = []byte(os.ExpandEnv(string(b)))
 
-	return yaml.Unmarshal(b, obj)
+	var cfg Config
+	err := yaml.Unmarshal(b, &cfg)
+
+	return cfg, err
 }
